b1c4c0382432ea5ecfd575886d39a2598c549308
Sebastian Scherbes <github@scherbes.de>
2023-03-04 17:23:04 +0800

    create home, calendar, locations pages

---
+++ pages/calendar.tsx
@@ -0,0 +1,5 @@
+import { Title } from "@mantine/core";
+
+export default function Calendar() {
+  return <Title>Kalender</Title>;
+}
---
+++ pages/locations.tsx
@@ -0,0 +1,5 @@
+import { Title } from "@mantine/core";
+
+export default function Locations() {
+  return <Title>Locations</Title>;
+}
