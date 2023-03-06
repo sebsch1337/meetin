14edcaf6339d5bba79a1354c77a8eb9b24d8452d
Sebastian Scherbes <github@scherbes.de>
2023-03-06 19:55:08 +0800

    create dummy data

---
+++ dummy.js
@@ -0,0 +1,114 @@
+export const db = [
+  {
+    id: 1,
+    name: "Wenkers",
+    description: "Alte Dortmunder Traditionskneipe mit leckerem Bier und gutem Essen.",
+    tags: ["Restaurant", "Indoor", "Outdoor"],
+    maxCapacity: 60,
+    visits: [
+      { visitors: 23, date: 1647580800, notes: "Viele Stammgäste da" }, // March 17, 2022
+      { visitors: 14, date: 1658342400, notes: "Ruhiger Abend" }, // July 21, 2022
+    ],
+  },
+  {
+    id: 2,
+    name: "Die Schenke",
+    description: "Tolle Studikneipe. Gibts leider nicht mehr.",
+    tags: ["Bar", "Indoor", "Outdoor", "Cocktails"],
+    maxCapacity: 30,
+    visits: [
+      { visitors: 13, date: 1650758400, notes: "Viele Studenten und junge Leute unterwegs" }, // April 21, 2022
+      { visitors: 47, date: 1665734400, notes: "Volles Haus wegen Konzert in der Nähe" }, // October 13, 2022
+    ],
+  },
+  {
+    id: 3,
+    name: "Biergarten am See",
+    description: "Schöner Biergarten direkt am See mit Blick auf die Berge.",
+    tags: ["Biergarten", "Outdoor"],
+    maxCapacity: 100,
+    visits: [
+      { visitors: 65, date: 1652563200, notes: "Sonniges Wetter, viele Familien mit Kindern" }, // May 12, 2022
+      { visitors: 43, date: 1663324800, notes: "Viele Radfahrer und Wanderer auf Tour" }, // September 15, 2022
+      { visitors: 88, date: 1670265600, notes: "Sonderveranstaltung mit Live-Musik" }, // March 31, 2023
+    ],
+  },
+  {
+    id: 4,
+    name: "Cocktailbar Deluxe",
+    description: "Stilvolle Cocktailbar mit exotischen Drinks und gemütlicher Atmosphäre.",
+    tags: ["Bar", "Indoor", "Cocktails"],
+    maxCapacity: 50,
+    visits: [
+      { visitors: 29, date: 1654963200, notes: "Viele Singles und Paare unterwegs" }, // June 9, 2022
+      { visitors: 37, date: 1664515200, notes: "Beliebter Treffpunkt für Geschäftsleute nach Feierabend" }, // September 29, 2022
+      { visitors: 18, date: 1666724800, notes: "Wegen Corona nur reduzierte Öffnungszeiten" }, // November 3, 2022
+    ],
+  },
+  {
+    id: 5,
+    name: "Brewery Bistro",
+    description: "Cozy bistro with handcrafted beers and delicious pub food.",
+    tags: ["Restaurant", "Brewery", "Indoor"],
+    maxCapacity: 40,
+    visits: [
+      { visitors: 19, date: 1671302400, notes: "Beer tasting event" }, // March 9, 2023
+      { visitors: 24, date: 1674883200, notes: "Trivia night" }, // April 27, 2023
+    ],
+  },
+  {
+    id: 6,
+    name: "Rooftop Lounge",
+    description: "Stunning views of the city skyline and creative cocktails.",
+    tags: ["Bar", "Outdoor", "Cocktails"],
+    maxCapacity: 20,
+    visits: [
+      { visitors: 12, date: 1669123200, notes: "Chilly weather, but heaters were provided" }, // March 2, 2023
+      { visitors: 18, date: 1676486400, notes: "Celebrity sighting!" }, // June 15, 2023
+    ],
+  },
+  {
+    id: 7,
+    name: "Seafood Shack",
+    description: "Fresh seafood dishes in a casual, beach-themed atmosphere.",
+    tags: ["Restaurant", "Seafood", "Indoor", "Outdoor"],
+    maxCapacity: 60,
+    visits: [
+      { visitors: 35, date: 1672099200, notes: "Large family gathering" }, // March 23, 2023
+      { visitors: 41, date: 1678464000, notes: "Surprise birthday party" }, // June 29, 2023
+    ],
+  },
+  {
+    id: 8,
+    name: "Whiskey Bar",
+    description: "Extensive whiskey selection and rustic decor.",
+    tags: ["Bar", "Indoor", "Whiskey"],
+    maxCapacity: 30,
+    visits: [
+      { visitors: 16, date: 1672915200, notes: "Whiskey tasting flight" }, // March 30, 2023
+      { visitors: 25, date: 1680643200, notes: "Live music performance" }, // October 5, 2023
+    ],
+  },
+  {
+    id: 9,
+    name: "Pizzeria Paradiso",
+    description: "Authentic Italian-style pizza and casual dining.",
+    tags: ["Restaurant", "Pizza", "Indoor"],
+    maxCapacity: 50,
+    visits: [
+      { visitors: 31, date: 1673721600, notes: "Large group of college friends" }, // April 6, 2023
+      { visitors: 22, date: 1681507200, notes: "Date night for many couples" }, // January 12, 2024
+    ],
+  },
+  {
+    id: 10,
+    name: "Barnyard BBQ",
+    description: "Finger-lickin' good BBQ and rustic ambiance.",
+    tags: ["Restaurant", "BBQ", "Indoor", "Outdoor"],
+    maxCapacity: 70,
+    visits: [
+      { visitors: 52, date: 1675329600, notes: "Company team-building event" }, // April 27, 2023
+      { visitors: 45, date: 1684166400, notes: "Graduation celebration" }, // May 11,
+    ],
+  },
+];
