6e5f1166fc5e521c76f5b839f0682a2376c7d11f
Sebastian Scherbes <github@scherbes.de>
2023-03-09 17:24:08 +0800

    change noGo indicator

--- components/LocationCard/index.tsx
+++ components/LocationCard/index.tsx
@@ -11,12 +11,11 @@ import {
   Accordion,
   Button,
   ActionIcon,
-  Indicator,
 } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
 import Link from "next/link";
 
-import { IconDownload, IconHome, IconSun } from "@tabler/icons-react";
+import { IconDownload, IconHome, IconInfoCircle, IconSun } from "@tabler/icons-react";
 
 import { Location } from "../../dbLocations";
 
@@ -30,119 +29,117 @@ export default function LocationCard({
   averageVisitors: String;
 }) {
   return (
-    <Indicator color="red" size={25} withBorder disabled={!location.noGo} key={location.id}>
-      <Card w={350} mih={550} shadow="sm" padding="xl">
-        <Card.Section>
-          <Carousel
-            withIndicators
-            maw={350}
-            styles={{
-              control: {
-                "&[data-inactive]": {
-                  opacity: 0,
-                  cursor: "default",
-                },
-              },
-            }}
-          >
-            {location.images.map((image) => (
-              <Carousel.Slide key={image}>
-                <Image
-                  src={image}
-                  width={350}
-                  height={200}
-                  alt={`Bild von ${location.name}`}
-                  style={{ position: "relative" }}
-                />
-                <Link href={image} download={location.name}>
-                  <ActionIcon variant="filled" pos={"absolute"} top={10} right={10}>
-                    <IconDownload size="1.125rem" />
-                  </ActionIcon>
-                </Link>
-              </Carousel.Slide>
-            ))}
-          </Carousel>
-        </Card.Section>
-
-        <Title order={2} weight={500} size={"h3"} mt="xs">
-          {location.name}
-        </Title>
-        <Text size={"xs"}>Innenstadt West</Text>
-
-        <SimpleGrid cols={2} verticalSpacing="xs" mt={"xs"}>
-          <div>
-            <Text size={"sm"}>Mittelwert Besucher</Text>
-            <Text color="dimmed" size="sm">
-              {averageVisitors}
-            </Text>
-          </div>
-          <div>
-            <Text size={"sm"}>Letzter Besuch</Text>
-            <Text color="dimmed" size="sm">
-              {lastVisitedDay}
-            </Text>
-          </div>
-
-          <div>
-            <Text size={"sm"}>Max. Besucher</Text>
-            <Text color="dimmed" size="sm">
-              {location.maxCapacity}
-            </Text>
-          </div>
-          <div>
-            <Text size={"sm"}>Indoor / Outdoor</Text>
-            <Center inline>
-              <IconHome color={location.indoor ? "teal" : "grey"} size={18} />
-              <Space w="xs" />
-              {"/"}
-              <Space w="xs" />
-              <IconSun color={location.outdoor ? "yellow" : "grey"} size={18} />
-            </Center>
-          </div>
-        </SimpleGrid>
-
-        <Accordion
-          variant="filled"
+    <Card w={350} mih={550} shadow="sm" padding="xl" key={location.id}>
+      <Card.Section>
+        <Carousel
+          withIndicators
+          maw={350}
           styles={{
-            control: { paddingInline: 0, paddingTop: 15, paddingBottom: 5 },
-            content: { padding: 0 },
+            control: {
+              "&[data-inactive]": {
+                opacity: 0,
+                cursor: "default",
+              },
+            },
           }}
         >
-          <Accordion.Item value="description">
-            <Accordion.Control>
-              <Text size={"sm"} weight={500}>
-                Beschreibung
-              </Text>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <Text size={"sm"}>{location.description}</Text>
-            </Accordion.Panel>
-          </Accordion.Item>
-
-          <Accordion.Item value="information">
-            <Accordion.Control>
-              <Text size={"sm"} weight={500}>
-                Interne Informationen
-              </Text>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <Text size={"sm"}>{location.infos}</Text>
-            </Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-
-        <Group mt={"md"} mb={"md"} spacing={"xs"} mih={45}>
-          {location?.tags.map((tag) => (
-            <Badge variant={"outline"} key={tag} size={"xs"}>
-              {tag}
-            </Badge>
+          {location.images.map((image) => (
+            <Carousel.Slide key={image}>
+              <Image
+                src={image}
+                width={350}
+                height={200}
+                alt={`Bild von ${location.name}`}
+                style={{ position: "relative" }}
+              />
+              <Link href={image} download={location.name}>
+                <ActionIcon variant="filled" pos={"absolute"} top={10} right={10}>
+                  <IconDownload size="1.125rem" />
+                </ActionIcon>
+              </Link>
+            </Carousel.Slide>
           ))}
-        </Group>
+        </Carousel>
+      </Card.Section>
 
-        <Button variant="light" color="blue" fullWidth mt="xl" radius="md">
-          Bearbeiten
-        </Button>
-      </Card>
-    </Indicator>
+      <Title order={2} weight={500} size={"h3"} mt="xs" color={location.noGo ? "red" : ""}>
+        {location.name} {location.noGo && <IconInfoCircle size={15} />}
+      </Title>
+      <Text size={"xs"}>Innenstadt West</Text>
+
+      <SimpleGrid cols={2} verticalSpacing="xs" mt={"xs"}>
+        <div>
+          <Text size={"sm"}>Mittelwert Besucher</Text>
+          <Text color="dimmed" size="sm">
+            {averageVisitors}
+          </Text>
+        </div>
+        <div>
+          <Text size={"sm"}>Letzter Besuch</Text>
+          <Text color="dimmed" size="sm">
+            {lastVisitedDay}
+          </Text>
+        </div>
+
+        <div>
+          <Text size={"sm"}>Max. Besucher</Text>
+          <Text color="dimmed" size="sm">
+            {location.maxCapacity}
+          </Text>
+        </div>
+        <div>
+          <Text size={"sm"}>Indoor / Outdoor</Text>
+          <Center inline>
+            <IconHome color={location.indoor ? "teal" : "grey"} size={18} />
+            <Space w="xs" />
+            {"/"}
+            <Space w="xs" />
+            <IconSun color={location.outdoor ? "yellow" : "grey"} size={18} />
+          </Center>
+        </div>
+      </SimpleGrid>
+
+      <Accordion
+        variant="filled"
+        styles={{
+          control: { paddingInline: 0, paddingTop: 15, paddingBottom: 5 },
+          content: { padding: 0 },
+        }}
+      >
+        <Accordion.Item value="description">
+          <Accordion.Control>
+            <Text size={"sm"} weight={500}>
+              Beschreibung
+            </Text>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <Text size={"sm"}>{location.description}</Text>
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="information">
+          <Accordion.Control>
+            <Text size={"sm"} weight={500}>
+              Interne Informationen
+            </Text>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <Text size={"sm"}>{location.infos}</Text>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+
+      <Group mt={"md"} mb={"md"} spacing={"xs"} mih={45}>
+        {location?.tags.map((tag) => (
+          <Badge variant={"outline"} key={tag} size={"xs"}>
+            {tag}
+          </Badge>
+        ))}
+      </Group>
+
+      <Button variant="light" color="blue" fullWidth mt="xl" radius="md">
+        Bearbeiten
+      </Button>
+    </Card>
   );
 }
--- pages/locations.tsx
+++ pages/locations.tsx
@@ -1,42 +1,8 @@
 import LocationCard from "@/components/LocationCard";
 import { Group, Space, Title } from "@mantine/core";
-import { db } from "../dbLocations";
-
-const dummyData = db;
-
-interface Visit {
-  visitors: number;
-  date: number;
-  notes: string;
-}
-
-const getLastVisit = (visits: Visit[]) =>
-  visits?.length > 0 ? visits.reduce((prev, current) => (prev.date > current.date ? prev : current)) : false;
-
-const getLastVisitedDay = (visits: Visit[]): string => {
-  const lastVisit = getLastVisit(visits);
-  if (lastVisit) {
-    return new Date(lastVisit.date * 1000).toLocaleDateString("de-DE", { dateStyle: "long" });
-  } else {
-    return "Nie";
-  }
-};
-
-const getAverageVisitors = (visits: Visit[]): string => {
-  if (!visits || visits?.length === 0) {
-    return "N/A";
-  }
-  const averageVisitors = Math.round(
-    visits
-      .slice(-3)
-      .map((visit) => visit.visitors)
-      .reduce((a, b) => a + b) / visits.length
-  );
-  const minVisitors = Math.min(...visits.slice(-3).map((visit) => visit.visitors));
-  const maxVisitors = Math.max(...visits.slice(-3).map((visit) => visit.visitors));
-
-  return `${averageVisitors} (${minVisitors} / ${maxVisitors})`;
-};
+import { dbLocations } from "../dbLocations";
+import { getLastVisitedDay, getAverageVisitors } from "@/utils/visit";
+const dummyData = dbLocations;
 
 export default function Locations() {
   return (
