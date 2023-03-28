d12eb82cc27d7dbeabb69f6524842148f3955ed2
Sebastian Scherbes <github@scherbes.de>
2023-03-28 12:23:21 +0800

    move delete api call to index

--- pages/api/images/index.tsx
+++ pages/api/images/index.tsx
@@ -1,4 +1,8 @@
-import { createCloudinarySignature, uploadImageToCloudinary } from "@/services/cloudinary";
+import {
+  createCloudinarySignature,
+  uploadImageToCloudinary,
+  deleteImageFromCloudinary,
+} from "@/services/cloudinary";
 
 export default async function handler(req: any, res: any): Promise<any> {
   const {
@@ -24,6 +28,19 @@ export default async function handler(req: any, res: any): Promise<any> {
       try {
         const uploadedImage: any = await uploadImageToCloudinary(req.body);
         res.status(200).json(uploadedImage);
+      } catch (error: any) {
+        if (error.status) {
+          return res.status(error.status).json({ message: error.message });
+        }
+        console.error(error.message);
+        return res.status(500).json({ message: "internal server error" });
+      }
+      break;
+
+    case "DELETE":
+      try {
+        const deletedImage: any = await deleteImageFromCloudinary(publicId);
+        res.status(200).json(deletedImage);
       } catch (error: any) {
         if (error.status) {
           return res.status(error.status).json({ message: error.message });
