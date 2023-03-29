2c247ff7831fbcfbcd112d8df776545f398736a8
Sebastian Scherbes <github@scherbes.de>
2023-03-29 18:40:57 +0800

    create delete-image function

--- pages/locations.tsx
+++ pages/locations.tsx
@@ -1,25 +1,29 @@
 import LocationCard from "@/components/LocationCard";
-import { Button, Flex, Group, Modal, Space, Title } from "@mantine/core";
+import { Button, Flex, Group, Modal, Space, Title, Text } from "@mantine/core";
 import { dbEvents } from "../dbEvents";
 import { getLastVisitedDay, getAverageVisitors } from "@/utils/visit";
 
-import { useDisclosure, useLocalStorage } from "@mantine/hooks";
+import { useDisclosure } from "@mantine/hooks";
 import { IconCheck, IconPlus } from "@tabler/icons-react";
 
 import { nanoid } from "nanoid";
 import { notifications } from "@mantine/notifications";
 import LocationForm from "@/components/LocationForm";
 import { useState } from "react";
+import { uploadImageToCloudinary } from "@/services/cloudinary";
+
+import useLocalStorage from "@/utils/useLocalStorage";
 
 export default function Locations() {
-  const [locations, setLocations] = useLocalStorage({ key: "dbLocations", defaultValue: [] });
-  const [events] = useLocalStorage(dbEvents);
+  const [locations, setLocations] = useLocalStorage("dbLocations", []);
+  const [events] = useLocalStorage("dbEvents", dbEvents ?? []);
   const [modalOpened, { open: openModal, close: closeModal }] = useDisclosure(false);
+  const [modal, setModal] = useState({ title: "", details: false, deleteImage: false });
   const [editLocationMode, setEditLocationMode] = useState(false);
   const [preValues, setPreValues] = useState({});
 
-  const createLocation = (values: any, images: string[] = []) => {
-    setLocations((prevLocations): any => [
+  const createLocation = (values: any, images: any[] = []) => {
+    setLocations((prevLocations: any) => [
       ...prevLocations,
       {
         id: nanoid(4),
@@ -52,8 +56,8 @@ export default function Locations() {
     });
   };
 
-  const editLocation = (values: any, locationId: string, images: string[] = []) => {
-    setLocations((prevLocations): any => {
+  const editLocation = (values: any, locationId: string, images: any[] = []) => {
+    setLocations((prevLocations: any) => {
       const locationToChange: any = prevLocations?.find((location: any) => location.id === locationId);
       locationToChange.name = values?.name;
       locationToChange.address.road = values?.road;
@@ -112,25 +116,38 @@ export default function Locations() {
     return await fetch("api/images/?publicId=" + deleteImageId, { method: "DELETE" });
   };
 
+  const uploadImageHandler = async (images: any[], locationId: string) => {
+    try {
+      const uploadedImageData: any[] = await Promise.all(
+        images.map(async (image: any) => uploadImageToCloudinary(image))
+      );
+
+      setLocations((prevLocations: any) => {
+        const locationToChange: any = prevLocations?.find((location: any) => location.id === locationId);
+        locationToChange.images = [...locationToChange?.images, ...uploadedImageData];
+        return prevLocations;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Title>Locations</Title>
       <Space h={"md"} />
       <Group position={"apart"}>
-        <Modal
-          opened={modalOpened}
-          onClose={closeModal}
-          title={editLocationMode ? "Location bearbeiten" : "Neue Location"}
-          centered
-        >
-          <LocationForm
-            closeModal={closeModal}
-            createLocation={createLocation}
-            editLocation={editLocation}
-            editLocationMode={editLocationMode}
-            preValues={preValues}
-            deleteImage={deleteImage}
-          />
+        <Modal opened={modalOpened} onClose={closeModal} title={modal.title} centered>
+          {modal.details && (
+            <LocationForm
+              closeModal={closeModal}
+              createLocation={createLocation}
+              editLocation={editLocation}
+              editLocationMode={editLocationMode}
+              preValues={preValues}
+            />
+          )}
+          {modal.deleteImage && <Text size={"sm"}>Möchtest du dieses Bild löschen?</Text>}
         </Modal>
 
         <Button
@@ -141,6 +158,11 @@ export default function Locations() {
           onClick={() => {
             setEditLocationMode(false);
             setPreValues({});
+            setModal({
+              title: "Neue Location",
+              details: true,
+              deleteImage: false,
+            });
             openModal();
           }}
         >
@@ -157,7 +179,10 @@ export default function Locations() {
             key={location.id}
             setEditLocationMode={setEditLocationMode}
             setPreValues={setPreValues}
+            setModal={setModal}
             openModal={openModal}
+            uploadImageHandler={uploadImageHandler}
+            deleteImage={deleteImage}
           />
         ))}
       </Flex>
