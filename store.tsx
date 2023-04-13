9fc627184890586add11935a7a62b69120551faf
Sebastian Scherbes <github@scherbes.de>
2023-04-13 09:16:23 +0700

    remove return type from eventAtom

--- store.tsx
+++ store.tsx
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 
 export const locationsAtom = atom<Location[]>([]);
 
-export const eventsAtom: any = atom<Event[]>([]);
+export const eventsAtom = atom<Event[]>([]);
 
 export const modalAtom = atom<Modal>({
   title: "",
