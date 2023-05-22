1ca458288ccb2910e3b02ff473ecd07bb82d1776
Sebastian Scherbes <github@scherbes.de>
2023-05-22 21:01:09 +0800

    create fiveLeastVisitedLocations function

--- lib/visitLib.tsx
+++ lib/visitLib.tsx
@@ -4,6 +4,7 @@ export const getEventsByLocationId = (locationId: string, events: Event[]): any[
   events?.length > 0 ? events?.filter((event) => event.locationId === locationId) : [];
 
 export const getLastVisit = (locationId: string, events: Event[]): any => {
+  // console.log(events)
   const locationEvents = getEventsByLocationId(locationId, events);
   return locationEvents?.length > 0
     ? locationEvents.reduce((prev: any, current: any) => (prev.dateTime > current.dateTime ? prev : current))
@@ -14,6 +15,24 @@ export const getLastVisitedDay = (locationId: any, events: Event[]): string => {
   const lastVisit = getLastVisit(locationId, events);
 
   return lastVisit ? getLocalDateLong(lastVisit.dateTime) : "Nie";
+};
+
+export const getFiveLeastVisitedLocations = (locations: Location[], events: Event[]): string[] => {
+  const locationLastDates: any[] =
+    locations?.length > 0
+      ? locations.map((location: any) => ({
+          id: location.id,
+          lastVisited: getLastVisit(location.id, events).dateTime ?? null,
+        }))
+      : [];
+
+  return locationLastDates
+    .sort(
+      (prev: any, current: any) =>
+        new Date(prev.lastVisited).getTime() - new Date(current.lastVisited).getTime()
+    )
+    .slice(0, 5)
+    .map((location: any) => location.id);
 };
 
 export const getAverageVisitors = (locationId: string, events: Event[]): number => {
