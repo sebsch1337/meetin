3dc252855cceb67791bb5f8591c719b9eeb5b9ac
Sebastian Scherbes <github@scherbes.de>
2023-05-30 15:22:19 +0800

    create DetailsModal component

---
+++ components/DetailsModal/index.tsx
@@ -0,0 +1,26 @@
+import { Modal } from "@mantine/core";
+
+export default function DetailsModal({
+  opened,
+  onClose,
+  modal,
+  children,
+}: {
+  opened: boolean;
+  onClose: any;
+  modal: Modal;
+  children: any;
+}) {
+  return (
+    <Modal.Root opened={opened} onClose={onClose}>
+      <Modal.Overlay />
+      <Modal.Content>
+        <Modal.Header style={{ zIndex: 200 }} px={0} mx={"md"}>
+          <Modal.Title>{modal.title}</Modal.Title>
+          <Modal.CloseButton />
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+      </Modal.Content>
+    </Modal.Root>
+  );
+}
