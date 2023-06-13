3deb054d7abf8440b6986e9941301a05897166aa
Sebastian Scherbes <github@scherbes.de>
2023-06-13 08:57:03 +0800

    fix delete button text in events form

--- components/EventForm/index.tsx
+++ components/EventForm/index.tsx
@@ -187,7 +187,7 @@ export default function EventForm({
                 }));
               }}
             >
-              Location löschen
+              Event löschen
             </Button>
           </>
         )}
