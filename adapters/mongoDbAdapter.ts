d70e82147677669e4c1416090a720ce0bbaffa44
Sebastian Scherbes <github@scherbes.de>
2023-07-13 09:27:13 +0200

    change mongoDbAdapter to TypeScript

--- adapters/mongoDbAdapter.ts
+++ adapters/mongoDbAdapter.ts
@@ -1,6 +1,10 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
 
+declare const global: {
+  _mongoClientPromise?: Promise<MongoClient>;
+};
+
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
@@ -8,9 +12,8 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise;
-// let clientPromise: Promise<MongoClient>;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
