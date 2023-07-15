1ed295159d44919ecc34dd14be6214a24f22a303
Sebastian Scherbes <github@scherbes.de>
2023-07-15 15:38:18 +0200

    create getTeam functions

---
+++ services/teamService.tsx
@@ -0,0 +1,71 @@
+import dbConnect from "../lib/dbConnect";
+import Teams from "../models/teamsModel";
+import { sanitizeTeam, validateTeam } from "@/validators/teamValidator";
+
+/**
+ * Gets all teams from the database.
+ *
+ * @returns An array of sanitized and validated team objects.
+ * @throws Error if the team array is not found or not an array.
+ */
+export async function getAllTeamsFromDb(): Promise<any> {
+  await dbConnect();
+
+  const teams = await Teams.find({});
+  if (!Array.isArray(teams)) {
+    const error: any = new Error("No teams found");
+    error.status = 404;
+    throw error;
+  }
+
+  const sanitizedTeams = await Promise.all(teams.map(async (team) => await validateTeam(sanitizeTeam(team))));
+
+  return sanitizedTeams;
+}
+
+/**
+ * Retrieves an team from the database based on the provided team ID.
+ * @param teamId - The ID of the team to retrieve.
+ * @returns A promise that resolves to the retrieved team.
+ * @throws If the team is not found in the database.
+ */
+export async function getTeamByIdFromDb(teamId: string): Promise<any> {
+  await dbConnect();
+
+  const sanitizedInput = await validateTeam(sanitizeTeam({ id: teamId }));
+
+  const team = await Teams.findById(sanitizedInput.id).exec();
+  if (!team) {
+    const error: any = new Error("Team not found");
+    error.status = 404;
+    throw error;
+  }
+
+  const sanitizedTeam = await validateTeam(sanitizeTeam(team));
+
+  return sanitizedTeam;
+}
+
+/**
+ * Retrieves an team from the database based on the provided team name.
+ * @param teamName - The name of the team to retrieve.
+ * @returns A promise that resolves to the retrieved team.
+ * @throws Error if the team is not found in the database.
+ */
+export async function getTeamByNameFromDb(teamName: string): Promise<any> {
+  await dbConnect();
+
+  const sanitizedInput = await validateTeam(sanitizeTeam({ name: teamName.replaceAll("+", " ") }));
+
+  const team: any = await Teams.findOne({ name: { $regex: new RegExp("^" + sanitizedInput?.name?.toLowerCase() + "$", "i") } }).exec();
+
+  if (!team) {
+    const error: any = new Error("Team not found");
+    error.status = 200;
+    throw error;
+  }
+
+  const sanitizedTeam = await validateTeam(sanitizeTeam(team));
+
+  return sanitizedTeam;
+}
