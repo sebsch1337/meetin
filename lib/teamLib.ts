2ac64bd1e64bbbfceeb3becc6d4f6f4d9b4c4977
Sebastian Scherbes <github@scherbes.de>
2023-07-15 15:37:09 +0200

    create getTeamByName function

---
+++ lib/teamLib.ts
@@ -0,0 +1,9 @@
+export const getTeamByName = async (teamName: string): Promise<any> => {
+  const sanitizedTeamName = teamName.replaceAll(" ", "+");
+  const response = await fetch(`/api/teams/search?teamname=${sanitizedTeamName}`, {
+    method: "GET",
+  });
+
+  const data = await response.json();
+  return data;
+};
