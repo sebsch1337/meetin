249223ff6579b6db01a718c7a06dde42e5ab7c5f
Sebastian Scherbes <github@scherbes.de>
2023-07-15 15:36:03 +0200

    add WelcomeInvited component

---
+++ components/WelcomeInvited/index.tsx
@@ -0,0 +1,46 @@
+import { Blockquote, Button, Group, Text } from "@mantine/core";
+import { IconComet } from "@tabler/icons-react";
+
+export default function WelcomeInvited({
+  invitedTeam,
+  setButtonDisabled,
+  setInvitationState,
+}: {
+  invitedTeam: Team;
+  setButtonDisabled: any;
+  setInvitationState: any;
+}) {
+  return (
+    <Blockquote p={"sm"} icon={<IconComet size={"2rem"} />}>
+      <Text>
+        Du wurdest zum Team
+        <Text span fw={700}>
+          {` ${invitedTeam?.name} `}
+        </Text>
+        eingeladen!
+      </Text>
+      <Group mt={"sm"}>
+        <Button
+          size="xs"
+          fullWidth
+          onClick={() => {
+            setButtonDisabled(false);
+            setInvitationState("accepted");
+          }}
+        >
+          Einladung annehmen
+        </Button>
+        <Button
+          size="xs"
+          fullWidth
+          variant="outline"
+          onClick={() => {
+            setInvitationState("declined");
+          }}
+        >
+          Einladung ablehnen
+        </Button>
+      </Group>
+    </Blockquote>
+  );
+}
