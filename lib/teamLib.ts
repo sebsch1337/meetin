3a7d861067dea211588b84e203742f44c9c33634
Sebastian Scherbes <github@scherbes.de>
2023-07-15 16:55:33 +0200

    add function descriptions for teamLib

--- lib/teamLib.ts
+++ lib/teamLib.ts
@@ -1,3 +1,9 @@
+/**
+ * Retrieves a team from the API based on the provided team name.
+ *
+ * @param teamName The name of the team used to search for the team.
+ * @returns A Promise that resolves to the team data retrieved from the API.
+ */
 export const getTeamByName = async (teamName: string): Promise<any> => {
   const sanitizedTeamName = teamName.replaceAll(" ", "+");
   const response = await fetch(`/api/teams/search?teamname=${sanitizedTeamName}`, {
@@ -8,6 +14,12 @@ export const getTeamByName = async (teamName: string): Promise<any> => {
   return data;
 };
 
+/**
+ * Retrieves a team from the API based on the provided invited email address.
+ *
+ * @param eMail The email address used to search for the team invitation.
+ * @returns A Promise that resolves to the team data retrieved from the API.
+ */
 export const getTeamByInvitedEmail = async (eMail: string): Promise<any> => {
   const response = await fetch(`/api/teams/search?invitedemail=${eMail}`, {
     method: "GET",
