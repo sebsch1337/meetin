4c391dd713a93f244cf756b166440e3a088f9860
Sebastian Scherbes <github@scherbes.de>
2023-07-19 20:37:38 +0200

    add invitedEmails array to validation

--- validators/teamValidator.ts
+++ validators/teamValidator.ts
@@ -1,27 +1,39 @@
 import { object, string, array } from "yup";
 
 export const sanitizeTeam = (team: any) => {
+  const sanitizedInvitation = {
+    email: team?.invitedEmails?.email?.toString().trim() || "",
+    role: team?.invitedEmails?.role?.toString().trim() || "",
+  };
+
   const sanitizedTeam = {
     id: team?._id?.toString().trim() || team?.id?.toString().trim() || undefined,
     name: team?.name?.trim() || "",
-    invitedEmails: team?.invitedEmails || [],
+    invitedEmails: team?.invitedEmails || {},
     admins: team?.admins || [],
     users: team?.users || [],
   };
 
-  return sanitizedTeam;
+  return { ...sanitizedTeam, invitedEmails: { ...sanitizedInvitation } };
 };
 
 export const validateTeam = async (team: any) => {
+  let teamInvitationSchema = object({
+    email: string().max(100),
+    role: string(),
+  });
+
   let teamSchema = object({
     id: string().length(24),
     name: string().max(50),
-    invitedEmails: array(string().max(100)).max(20).nullable(),
     admins: array(string().length(24)).max(20).nullable(),
     users: array(string().length(24)).max(20).nullable(),
   });
 
-  const validatedTeam = await teamSchema.validate(team);
+  const [validatedTeam, validatedTeamInvitation] = await Promise.all([
+    teamSchema.validate(team),
+    teamInvitationSchema.validate(team?.invitedEmails),
+  ]);
 
-  return validatedTeam;
+  return { ...validatedTeam, invitedEmails: [validatedTeamInvitation] };
 };
