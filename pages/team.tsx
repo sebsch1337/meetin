993e2ab873d51ff25c8786b6225cc2dc5fc611c2
Sebastian Scherbes <github@scherbes.de>
2023-07-19 20:57:17 +0200

    add team name to page

--- pages/team.tsx
+++ pages/team.tsx
@@ -9,7 +9,21 @@ import FormModal from "@/components/FormModal";
 
 import { IconPlus } from "@tabler/icons-react";
 
-export default function ManageTeam() {
+import { authOptions } from "./api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+import { getTeamByIdFromDb } from "@/services/teamService";
+
+export async function getServerSideProps(context: any) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (!session) return { redirect: { destination: "/login", permanent: false } };
+  if (!session?.user?.teamId) return { redirect: { destination: "/", permanent: false } };
+
+  const team = await getTeamByIdFromDb(session.user.teamId);
+
+  return { props: { teamName: team.name } };
+}
+
+export default function ManageTeam({ teamName }: { teamName: string }) {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const [modalOpened, { open: openModal, close: closeModal }] = useDisclosure(false);
   const [modal, setModal] = useState<Modal>();
@@ -35,7 +49,9 @@ export default function ManageTeam() {
 
       <Space h={"md"} />
 
-      <Title order={2}>Team {}</Title>
+      <Title order={2} size={22}>
+        {teamName}
+      </Title>
 
       <Space h={"md"} />
 
