349ac704210d087d9710846b7c47c67c5752b408
Sebastian Scherbes <github@scherbes.de>
2023-07-19 20:38:49 +0200

    add invitedEmails to type interface

--- types/teams.d.ts
+++ types/teams.d.ts
@@ -2,7 +2,12 @@ interface Team {
   id?: string;
   _id?: string;
   name?: string | undefined;
-  invitedEmails?: (string | undefined)[] | null | undefined;
+  invitedEmails?: InvitedEmails[] | null | undefined;
   admins?: (string | undefined)[] | null | undefined;
   users?: (string | undefined)[] | null | undefined;
+}
+
+interface InvitedEmails {
+  email: string;
+  role: string;
 }
