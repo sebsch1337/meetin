0b53e0e5b210a8f761f35c3f467550609f3e5140
Sebastian Scherbes <github@scherbes.de>
2023-07-21 16:10:29 +0200

    create deleteItemHandler function

--- components/InvitedMemberCard/index.tsx
+++ components/InvitedMemberCard/index.tsx
@@ -1,12 +1,29 @@
 import PaperCard from "../PaperCard";
 import MemberCardItem from "../MemberCardItem";
 import { Text } from "@mantine/core";
+import { declineInvitation, getTeamById } from "@/lib/teamLib";
 
-export default function InvitedMemberCard({ invitedMembers }: { invitedMembers: any }) {
+export default function InvitedMemberCard({
+  invitedMembers,
+  setInvitedMembers,
+  teamId,
+}: {
+  invitedMembers: any;
+  setInvitedMembers: any;
+  teamId: any;
+}) {
+  const deleteItemHandler = async (email: any, teamId: any) => {
+    await declineInvitation(email);
+    const newInvitedMembers = await getTeamById(teamId);
+    setInvitedMembers(newInvitedMembers);
+  };
+
   return (
     <PaperCard title="Eingeladene Mitglieder">
       {invitedMembers.length > 0 ? (
-        invitedMembers?.map((member: any) => <MemberCardItem key={member.email} member={member} />)
+        invitedMembers?.map((member: any) => (
+          <MemberCardItem key={member.email} member={member} deleteItem={() => deleteItemHandler(member.email, teamId)} />
+        ))
       ) : (
         <Text size={"sm"} fs={"italic"} c={"dimmed"}>
           Keine Einladungen vorhanden
