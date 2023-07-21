5f69992317351d01fb9caa9f88e39adf1cc3feb7
Sebastian Scherbes <github@scherbes.de>
2023-07-21 17:27:32 +0200

    create deleteItemHandler function

--- components/MemberCard/index.tsx
+++ components/MemberCard/index.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-
 import PaperCard from "../PaperCard";
 import MemberCardItem from "../MemberCardItem";
+import { getUsersAndAdminsForTeamId, removeUserFromTeam } from "@/lib/teamLib";
 
-export default function MemberCard({ team, members }: { team: Team; members: any[] }) {
-  const [teamMembers, setTeamMembers] = useState(members || []);
+export default function MemberCard({ teamId, teamMembers, setTeamMembers }: { teamId: any; teamMembers: any[]; setTeamMembers: any }) {
+  const deleteItemHandler = async (userId: any) => {
+    await removeUserFromTeam(userId);
+    const newMembers = await getUsersAndAdminsForTeamId(teamId);
+    console.log(newMembers);
+    setTeamMembers(newMembers);
+  };
 
   return (
     <PaperCard title={"Mitglieder verwalten"}>
       {teamMembers?.map((member: any) => (
-        <MemberCardItem key={member.id} member={member} deleteItem={() => {}} />
+        <MemberCardItem key={member.id} member={member} deleteItem={async () => await deleteItemHandler(member.id)} />
       ))}
     </PaperCard>
   );
