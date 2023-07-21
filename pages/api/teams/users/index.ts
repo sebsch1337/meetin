46a70b8a22005e4be8b90d2016a36e408ad9760e
Sebastian Scherbes <github@scherbes.de>
2023-07-21 17:29:15 +0200

    create team users api endpoint

---
+++ pages/api/teams/users/index.ts
@@ -0,0 +1,44 @@
+import {
+  getTeamByIdFromDb,
+  getTeamByInvitedEmailFromDb,
+  getTeamByNameFromDb,
+  getUsersAndAdminsForTeamFromDb,
+  removeUserIdFromTeamInDb,
+} from "@/services/teamService";
+
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../auth/[...nextauth]";
+import { getUserRoleInTeamFromDb } from "@/services/userService";
+
+export default async function handler(req: any, res: any): Promise<any> {
+  const {
+    query: {},
+    method,
+  } = req;
+
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) return res.status(401).json({ message: "unauthorized" });
+
+  const role = await getUserRoleInTeamFromDb(session.user?.id, session.user?.teamId);
+  if (role !== "admin") return res.status(403).json({ message: "forbidden" });
+
+  switch (method) {
+    case "GET":
+      try {
+        const users = await getUsersAndAdminsForTeamFromDb(session?.user?.teamId);
+        return res.status(200).json(users);
+      } catch (error: any) {
+        if (error.status) {
+          return res.status(error.status).json({ message: error.message });
+        }
+        console.error(error.message);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      break;
+
+    default:
+      res.status(405).json({ error: "Method not allowed" });
+      break;
+  }
+}
