d9a56a1a5e22c2204db46f9643c34914f1875483
Sebastian Scherbes <github@scherbes.de>
2023-07-21 12:54:39 +0200

    add getUserRoleInTeamFromDb function to teams page

--- pages/team.tsx
+++ pages/team.tsx
@@ -6,13 +6,17 @@ import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import AddMemberForm from "@/components/AddMemberForm";
 import MemberCard from "@/components/MemberCard";
 import FormModal from "@/components/FormModal";
+import InvitedMemberCard from "@/components/InvitedMemberCard";
 
 import { IconPlus } from "@tabler/icons-react";
 
 import { authOptions } from "./api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
+
 import { getTeamByIdFromDb, getUsersAndAdminsForTeamFromDb } from "@/services/teamService";
-import InvitedMemberCard from "@/components/InvitedMemberCard";
+import { getUserRoleInTeamFromDb } from "@/services/userService";
+
+import { createInvitation } from "@/lib/teamLib";
 
 export async function getServerSideProps(context: any) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -21,47 +25,52 @@ export async function getServerSideProps(context: any) {
 
   const team = await getTeamByIdFromDb(session.user.teamId);
   const members = await getUsersAndAdminsForTeamFromDb(session.user.teamId);
+  const userRole = await getUserRoleInTeamFromDb(session.user.id, session.user.teamId);
 
-  return { props: { team, members } };
+  return { props: { team, members, userRole } };
 }
 
-export default function ManageTeam({ team, members }: { team: Team; members: any[] }) {
+export default function ManageTeam({ team, members, userRole }: { team: Team; members: any[]; userRole: string }) {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const [modalOpened, { open: openModal, close: closeModal }] = useDisclosure(false);
   const [modal, setModal] = useState<Modal>();
 
+  const [invitedEmails, setInvitedEmails] = useState(team.invitedEmails);
+
   return (
     <Container fluid px={isMobile ? "xs" : "xl"} py={"md"}>
       <FormModal title={modal?.title} opened={modalOpened} close={closeModal}>
-        {modal?.type === "form" && <AddMemberForm />}
+        {modal?.type === "form" && (
+          <AddMemberForm createInvitation={createInvitation} closeModal={closeModal} setInvitedEmails={setInvitedEmails} />
+        )}
       </FormModal>
 
-      <Button
-        leftIcon={<IconPlus size="1rem" />}
-        variant={"light"}
-        size={"sm"}
-        color={"teal"}
-        onClick={() => {
-          setModal({ title: "Mitglied einladen", type: "form" });
-          openModal();
-        }}
-      >
-        Neues Mitglied
-      </Button>
-
-      <Space h={"md"} />
-
-      <Title order={2} size={22}>
-        {team.name}
-      </Title>
-
-      <Space h={"md"} />
-
-      <MemberCard team={team} members={members} />
-
-      <Space h={"md"} />
-
-      <InvitedMemberCard invitedMembers={team.invitedEmails} />
+      {userRole === "admin" ? (
+        <>
+          <Button
+            leftIcon={<IconPlus size="1rem" />}
+            variant={"light"}
+            size={"sm"}
+            color={"teal"}
+            onClick={() => {
+              setModal({ title: "Mitglied einladen", type: "form" });
+              openModal();
+            }}
+          >
+            Neues Mitglied
+          </Button>
+          <Space h={"md"} />
+          <Title order={2} size={22}>
+            {team.name}
+          </Title>
+          <Space h={"md"} />
+          <MemberCard team={team} members={members} />
+          <Space h={"md"} />
+          <InvitedMemberCard invitedMembers={invitedEmails} />
+        </>
+      ) : (
+        <Button>Team verlassen</Button>
+      )}
     </Container>
   );
 }
