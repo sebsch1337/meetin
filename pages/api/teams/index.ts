3aab38fa681386a7a0744f664cfc5ea683801105
Sebastian Scherbes <github@scherbes.de>
2023-07-23 20:32:08 +0200

    refactor try / catch for teams api

--- pages/api/teams/index.ts
+++ pages/api/teams/index.ts
@@ -12,36 +12,28 @@ export default async function handler(req: any, res: any): Promise<any> {
   const session = await getServerSession(req, res, authOptions);
   if (!session) return res.status(401).json({ message: "unauthorized" });
 
-  switch (method) {
-    case "GET":
-      try {
+  try {
+    switch (method) {
+      case "GET":
         const teams = await getAllTeamsFromDb();
         res.status(200).json(teams);
-      } catch (error: any) {
-        if (error.status) {
-          return res.status(error.status).json({ message: error.message });
-        }
-        console.error(error.message);
-        return res.status(500).json({ message: "internal server error" });
-      }
-      break;
+        break;
 
-    case "POST":
-      try {
+      case "POST":
         const teamData = JSON.parse(req.body);
         const team = await createTeamInDb(teamData.teamName, session?.user?.id);
         res.status(200).json(team);
-      } catch (error: any) {
-        if (error.status) {
-          return res.status(error.status).json({ message: error.message });
-        }
-        console.error(error.message);
-        return res.status(500).json({ message: "internal server error" });
-      }
-      break;
+        break;
 
-    default:
-      res.status(405).json({ error: "Method not allowed" });
-      break;
+      default:
+        res.status(405).json({ error: "Method not allowed" });
+        break;
+    }
+  } catch (error: any) {
+    if (error.status) {
+      return res.status(error.status).json({ message: error.message });
+    }
+    console.error(error.message);
+    return res.status(500).json({ message: "internal server error" });
   }
 }
