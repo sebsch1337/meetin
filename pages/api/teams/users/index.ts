3dd8120d7cd938a60c0beb1f82470be2fe7e67f3
Sebastian Scherbes <github@scherbes.de>
2023-07-23 11:36:17 +0200

    create api endpoint to change user role

--- pages/api/teams/users/index.ts
+++ pages/api/teams/users/index.ts
@@ -1,13 +1,7 @@
-import {
-  getTeamByIdFromDb,
-  getTeamByInvitedEmailFromDb,
-  getTeamByNameFromDb,
-  getUsersAndAdminsForTeamFromDb,
-  removeUserIdFromTeamInDb,
-} from "@/services/teamService";
-
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../auth/[...nextauth]";
+
+import { changeUserRoleInDb, getUsersAndAdminsForTeamFromDb } from "@/services/teamService";
 import { getUserRoleInTeamFromDb } from "@/services/userService";
 
 export default async function handler(req: any, res: any): Promise<any> {
@@ -22,23 +16,25 @@ export default async function handler(req: any, res: any): Promise<any> {
   const role = await getUserRoleInTeamFromDb(session.user?.id, session.user?.teamId);
   if (role !== "admin") return res.status(403).json({ message: "forbidden" });
 
-  switch (method) {
-    case "GET":
-      try {
+  try {
+    switch (method) {
+      case "GET":
         const users = await getUsersAndAdminsForTeamFromDb(session?.user?.teamId);
         return res.status(200).json(users);
-      } catch (error: any) {
-        if (error.status) {
-          return res.status(error.status).json({ message: error.message });
-        }
-        console.error(error.message);
-        return res.status(500).json({ message: "Internal server error" });
-      }
 
-      break;
+      case "POST":
+        const removedSuccess = await changeUserRoleInDb(req.body.userId, req.body.role);
+        return res.status(200).json({ message: `Changing role successfully: ${removedSuccess}` });
 
-    default:
-      res.status(405).json({ error: "Method not allowed" });
-      break;
+      default:
+        res.status(405).json({ error: "Method not allowed" });
+        break;
+    }
+  } catch (error: any) {
+    if (error.status) {
+      return res.status(error.status).json({ message: error.message });
+    }
+    console.error(error.message);
+    return res.status(500).json({ message: "Internal server error" });
   }
 }
