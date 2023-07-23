05e03e0f49cc03fac7f03febec9acaf988e2db5d
Sebastian Scherbes <github@scherbes.de>
2023-07-23 18:08:25 +0200

    add page reload when own id changed

--- components/MemberCard/index.tsx
+++ components/MemberCard/index.tsx
@@ -1,16 +1,31 @@
 import PaperCard from "../PaperCard";
 import MemberCardItem from "../MemberCardItem";
 import { changeUserRole, getUsersAndAdminsForTeamId, removeUserFromTeam } from "@/lib/teamLib";
+import { useRouter } from "next/router";
 
-export default function MemberCard({ teamId, teamMembers, setTeamMembers }: { teamId: any; teamMembers: any[]; setTeamMembers: any }) {
+export default function MemberCard({
+  teamId,
+  teamMembers,
+  setTeamMembers,
+  session,
+}: {
+  teamId: any;
+  teamMembers: any[];
+  setTeamMembers: any;
+  session: any;
+}) {
+  const router = useRouter();
+
   const deleteItemHandler = async (userId: any) => {
     await removeUserFromTeam(userId);
+    if (userId === session.user.id) router.push("/team");
     const newMembers = await getUsersAndAdminsForTeamId(teamId);
     setTeamMembers(newMembers);
   };
 
   const changeRoleHandler = async (userId: string, role: string) => {
     await changeUserRole(userId, role);
+    if (userId === session.user.id) router.push("/team");
     const newMembers = await getUsersAndAdminsForTeamId(teamId);
     setTeamMembers(newMembers);
   };
